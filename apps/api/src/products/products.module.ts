522b085cf974a730ecd3c216da54f96383b4c4b9
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-05 01:04:34 +0200

    Register product image uploads controller

--- apps/api/src/products/products.module.ts
+++ apps/api/src/products/products.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { WebhooksModule } from '../webhooks/webhooks.module';
+import { ProductImagesController } from './product-images.controller';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
   imports: [WebhooksModule],
-  controllers: [ProductsController],
+  controllers: [ProductsController, ProductImagesController],
   providers: [ProductsService],
   exports: [ProductsService],
 })
