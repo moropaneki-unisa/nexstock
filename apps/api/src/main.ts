c4177896f5046b5da31d56e5e05383b61bbbcb3a
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-04 23:35:06 +0200

    Fix cookie parser import for API startup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
-
+import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
