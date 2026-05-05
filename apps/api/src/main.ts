0e639d21aa5edf16d9549d15c3690d9076cc55c3
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-05 23:50:36 +0200

    fix: load cookie parser safely in production build

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+
+const cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
