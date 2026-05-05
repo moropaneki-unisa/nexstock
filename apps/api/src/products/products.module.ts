2b6d6faff01aa5e900b65a41893f6499784ec4bd
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-05 13:04:26 +0200

    Register product import export service

--- apps/api/src/products/products.module.ts
+++ apps/api/src/products/products.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { WebhooksModule } from '../webhooks/webhooks.module';
 import { ProductImagesController } from './product-images.controller';
 import { ProductsController } from './products.controller';
+import { ProductsImportExportService } from './products-import-export.service';
 import { ProductsService } from './products.service';
 
 @Module({
   imports: [WebhooksModule],
   controllers: [ProductsController, ProductImagesController],
-  providers: [ProductsService],
+  providers: [ProductsService, ProductsImportExportService],
   exports: [ProductsService],
 })
 export class ProductsModule {}
