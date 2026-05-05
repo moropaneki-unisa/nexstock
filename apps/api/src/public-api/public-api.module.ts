5ef5ee0d8c5f26727f7d9119e7ab6d5e5edf7bf6
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-05 23:47:10 +0200

    fix: use relative import for product fields module

--- apps/api/src/public-api/public-api.module.ts
+++ apps/api/src/public-api/public-api.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ApiKeysModule } from '../api-keys/api-keys.module';
+import { ProductFieldsModule } from '../product-fields/product-fields.module';
 import { ProductsModule } from '../products/products.module';
-import { PublicProductsController } from './public-products.controller';
-import { ProductFieldsModule } from 'src/product-fields/product-fields.module';
 import { PublicProductFieldsController } from './public-product-fields.controller';
+import { PublicProductsController } from './public-products.controller';
 
 @Module({
   imports: [ApiKeysModule, ProductsModule, ProductFieldsModule],
