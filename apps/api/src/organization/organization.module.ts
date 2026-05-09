13197955992be27a4149cb8cd586418e4bdac8b5
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-09 14:20:10 +0200

    Enable email service for organization invites

--- apps/api/src/organization/organization.module.ts
+++ apps/api/src/organization/organization.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { EmailModule } from '../email/email.module';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
 
 @Module({
+  imports: [EmailModule],
   controllers: [OrganizationController],
   providers: [OrganizationService],
 })
