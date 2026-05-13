5f5f08a47138dc98e1de1077206c685747c839e1
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-13 02:03:36 +0200

    feat: add shadcn collapsible primitive

---
+++ apps/web/components/ui/collapsible.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import * as CollapsiblePrimitive from "radix-ui"
+
+const Collapsible = CollapsiblePrimitive.Collapsible.Root
+const CollapsibleTrigger = CollapsiblePrimitive.Collapsible.Trigger
+const CollapsibleContent = CollapsiblePrimitive.Collapsible.Content
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
