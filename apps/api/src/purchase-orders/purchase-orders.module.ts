6236af8e35f756ecb365c882b4069e4092d89452
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-13 21:57:10 +0200

    Inject document templates into purchase orders

--- apps/api/src/purchase-orders/purchase-orders.module.ts
+++ apps/api/src/purchase-orders/purchase-orders.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DocumentTemplatesModule } from '../document-templates/document-templates.module';
 import { PurchaseOrdersController } from './purchase-orders.controller';
 import { PurchaseOrdersService } from './purchase-orders.service';
 
 @Module({
+  imports: [DocumentTemplatesModule],
   controllers: [PurchaseOrdersController],
   providers: [PurchaseOrdersService],
   exports: [PurchaseOrdersService],
