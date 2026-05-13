db832743ce6f47861f31cc4ca42d3d6d4ba61cd5
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-13 21:04:37 +0200

    Add new purchase order route

---
+++ apps/web/app/(dashboard)/purchase-orders/new/page.tsx
@@ -0,0 +1,21 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { PurchaseOrderFormContent } from "@/components/purchase-orders/purchase-order-form-content"
+import { SiteHeader } from "@/components/site-header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={{
+        "--sidebar-width": "calc(var(--spacing) * 72)",
+        "--header-height": "calc(var(--spacing) * 16)",
+      } as React.CSSProperties}
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <PurchaseOrderFormContent />
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
