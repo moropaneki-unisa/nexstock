d05c865cde5c6d275e38496d57f820db4db5122f
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-14 04:04:31 +0200

    Register product type API

--- apps/api/src/products/products.module.ts
+++ apps/api/src/products/products.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { WebhooksModule } from '../webhooks/webhooks.module';
 import { ProductImagesController } from './product-images.controller';
+import { ProductTypesController } from './product-types.controller';
+import { ProductTypesService } from './product-types.service';
 import { ProductsController } from './products.controller';
 import { ProductsImportExportService } from './products-import-export.service';
 import { ProductsService } from './products.service';
 
 @Module({
   imports: [WebhooksModule],
-  controllers: [ProductsController, ProductImagesController],
-  providers: [ProductsService, ProductsImportExportService],
-  exports: [ProductsService],
+  controllers: [ProductsController, ProductImagesController, ProductTypesController],
+  providers: [ProductsService, ProductsImportExportService, ProductTypesService],
+  exports: [ProductsService, ProductTypesService],
 })
 export class ProductsModule {}
