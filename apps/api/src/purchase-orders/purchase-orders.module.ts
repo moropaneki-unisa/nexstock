4ec105b8db287acda4516ad0ff05e854d6336d48
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-14 22:16:27 +0200

    Use shared email module for purchase orders

--- apps/api/src/purchase-orders/purchase-orders.module.ts
+++ apps/api/src/purchase-orders/purchase-orders.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentTemplatesModule } from '../document-templates/document-templates.module';
+import { EmailModule } from '../email/email.module';
 import { PurchaseOrdersController } from './purchase-orders.controller';
 import { PurchaseOrdersService } from './purchase-orders.service';
 
 @Module({
-  imports: [DocumentTemplatesModule],
+  imports: [DocumentTemplatesModule, EmailModule],
   controllers: [PurchaseOrdersController],
   providers: [PurchaseOrdersService],
   exports: [PurchaseOrdersService],
