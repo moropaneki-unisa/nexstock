67df29705e949663c876cdec7e14cac1398b5858
moropaneki-unisa <15264378@mylife.unisa.ac.za>
2026-05-14 20:53:42 +0200

    Register product supplier service

--- apps/api/src/products/products.module.ts
+++ apps/api/src/products/products.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WebhooksModule } from '../webhooks/webhooks.module';
 import { ProductImagesController } from './product-images.controller';
+import { ProductSuppliersService } from './product-suppliers.service';
 import { ProductTypesController } from './product-types.controller';
 import { ProductTypesService } from './product-types.service';
 import { ProductsController } from './products.controller';
@@ -10,7 +11,7 @@ import { ProductsService } from './products.service';
 @Module({
   imports: [WebhooksModule],
   controllers: [ProductsController, ProductImagesController, ProductTypesController],
-  providers: [ProductsService, ProductsImportExportService, ProductTypesService],
-  exports: [ProductsService, ProductTypesService],
+  providers: [ProductsService, ProductsImportExportService, ProductTypesService, ProductSuppliersService],
+  exports: [ProductsService, ProductTypesService, ProductSuppliersService],
 })
 export class ProductsModule {}
